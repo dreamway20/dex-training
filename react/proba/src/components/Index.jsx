141db52dabdd33ef27435624ae766ff2bc88c5c2
Liubov Altuhova <laltuhova@gmail.com>
2020-03-04 10:55:20 +0200

    4week router

---
+++ react/proba/src/components/Index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function Index() {
+    return (
+        <div className="home">
+            <article className="banner" id="banner">
+             
+              <img src="./image 2.png" alt="" width="974px" height="370px" />
+              <p>
+                Luca Doncic is the best NBA player ever ever ever and you can`t
+                beat it
+              </p>
+              Mexico was treated to a historic farewell on Saturday thanks to
+              the country’s first ever overtime thriller that capped off the
+              2019 NBA Mexico City Games. Kelly Oubre’s 3-pointer tied the score
+              at the end of regulation, then Patty Mills topped it with his
+              game-winning jumper with 0.3 seconds left in the extra period to
+              give San Antonio the victory. Here’s a look at the most memorable
+              sights and moments from the league’s 30th regular season game in
+              Mexico City.
+            </article>
+          </div>
+    );
+}
